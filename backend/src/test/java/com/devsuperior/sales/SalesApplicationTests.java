e7ff944c4e917b5d921bb542c426f209078f547d
Macedo <gemc@gft.com>
2022-09-07 16:16:38 -0300

    first commit

---
+++ backend/src/test/java/com/devsuperior/sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
