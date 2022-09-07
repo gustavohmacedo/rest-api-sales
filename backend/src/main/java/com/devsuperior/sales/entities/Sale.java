ad94235bf3038613dab6695e7d0830300360adfe
Macedo <gemc@gft.com>
2022-09-07 17:56:25 -0300

    criação da entidade Sale e confirguração do BD de teste

---
+++ backend/src/main/java/com/devsuperior/sales/entities/Sale.java
@@ -0,0 +1,24 @@
+package com.devsuperior.sales.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    public Sale() {
+    }
+
+}
